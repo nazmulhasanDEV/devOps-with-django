1aecb3bf49bfe424cc5bd7272854f99382f80562
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2025-09-06 20:36:07 +0600

    remove secret key

--- devops_with_djagno/settings.py
+++ devops_with_djagno/settings.py
@@ -4,9 +4,6 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-!y4he9*7#n=pdvz+33-j33ohf8kp$rod^cnwb%x0ebpndwi=^3"
 
