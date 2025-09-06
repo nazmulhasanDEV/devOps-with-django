834604c80bbbe88fe7be08a3fc71e05b3aed9a66
Nazmul Hasan <73014242+nazmulhasanDEV@users.noreply.github.com>
2025-09-06 21:10:22 +0600

    Update settings.py

--- devops_with_djagno/settings.py
+++ devops_with_djagno/settings.py
@@ -3,8 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-!y4he9*7#n=pdvz+33-j33ohf8kp$rod^cnwb%x0ebpndwi=^3"
 
 
