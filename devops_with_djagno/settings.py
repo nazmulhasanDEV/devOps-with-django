a84f8b416de6e1866b7957997990f88d39253e22
nazmulhasan7447 <nazmulhasan747406@gmail.com>
2025-09-06 20:35:25 +0600

    added comments

--- devops_with_djagno/settings.py
+++ devops_with_djagno/settings.py
@@ -3,6 +3,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-!y4he9*7#n=pdvz+33-j33ohf8kp$rod^cnwb%x0ebpndwi=^3"
 
 
