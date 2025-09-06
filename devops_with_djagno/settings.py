7a5aac17796e7fde52efe4a226de3eb42526febe
Nazmul Hasan <73014242+nazmulhasanDEV@users.noreply.github.com>
2025-09-06 21:10:22 +0600

    Update settings.py

--- devops_with_djagno/settings.py
+++ devops_with_djagno/settings.py
@@ -3,11 +3,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-!y4he9*7#n=pdvz+33-j33ohf8kp$rod^cnwb%x0ebpndwi=^3"
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -67,9 +62,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
